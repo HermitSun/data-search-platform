1e572d0c483130af958fe80c44cb0ca8359d4203
NJUleo <1250925329@qq.com>
2019-05-01 00:12:18 +0800

    后端架子

---
+++ backend/dsp/src/main/java/com/bitability/dsp/DataSearchPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.bitability.dsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSearchPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataSearchPlatformApplication.class, args);
+	}
+
+}
